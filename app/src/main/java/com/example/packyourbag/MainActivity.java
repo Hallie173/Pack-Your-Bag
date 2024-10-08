e6b457dcdd7164ec7dc465ebbf6ca258555e7b40
g15 5511 <phuonghaleduy@gmail.com>
2024-10-08 23:22:10 +0700

    Daily commit

--- app/src/main/java/com/example/packyourbag/MainActivity.java
+++ app/src/main/java/com/example/packyourbag/MainActivity.java
@@ -7,18 +7,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.packyourbag.Adapter.Adapter;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    List<String> titles;
+    List<Integer> images;
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        getSupportActionBar().hide();
     }
 }
