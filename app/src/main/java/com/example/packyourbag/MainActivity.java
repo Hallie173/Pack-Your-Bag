55c7657d095484c28537ccb7e5a8878d6c97ae62
Nguyên Chánh <128724767+Lebron000@users.noreply.github.com>
2024-10-16 20:41:06 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/packyourbag/MainActivity.java
+++ app/src/main/java/com/example/packyourbag/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.packyourbag;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,15 +15,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.packyourbag.Adapter.Adapter;
 import com.example.packyourbag.Constants.MyConstants;
+import com.example.packyourbag.Data.AppData;
+import com.example.packyourbag.Database.RoomDB;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     List<String> titles;
     List<Integer> images;
     Adapter adapter;
+    RoomDB database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
         addAllTitles();
         addAllImages();
+        persistAppData();
+        database = RoomDB.getInstance(this);
+        System.out.println("------------------------>" + database.mainDAO().getAllSelection(false).get(0).getItemname());
 
         adapter = new Adapter(this, titles, images, MainActivity.this);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
@@ -52,6 +61,24 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Tap back button in order to exit!", Toast.LENGTH_SHORT).show();
         }
         mBackPressed = System.currentTimeMillis();
+    }
+
+    private void persistAppData(){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        database = RoomDB.getInstance(this);
+        AppData appData = new AppData(database);
+        int last = prefs.getInt(AppData.LAST_VERSION, 0);
+        if (!prefs.getBoolean(MyConstants.FIRST_TIME_CAMEL_CASE, false)){
+            appData.persistAllData();
+            editor.putBoolean(MyConstants.FIRST_TIME_CAMEL_CASE, true);
+        } else if (last<AppData.NEW_VERSION) {
+            database.mainDAO().deleteAllSystemItems(MyConstants.SYSTEM_SMALL);
+            appData.persistAllData();
+            editor.putInt(AppData.LAST_VERSION, AppData.NEW_VERSION);
+            editor.commit();
+        }
     }
 
     private void addAllTitles() {
