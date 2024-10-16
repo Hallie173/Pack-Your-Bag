52fb5f33be0931689bbd310e0ce33940cfcfa31f
Nguyên Chánh <128724767+Lebron000@users.noreply.github.com>
2024-10-16 20:38:56 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/packyourbag/Database/RoomDB.java
@@ -0,0 +1,28 @@
+package com.example.packyourbag.Database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.packyourbag.DAO.ItemsDAO;
+import com.example.packyourbag.Models.Items;
+
+@Database(entities = Items.class, version = 1, exportSchema = false)
+public abstract class RoomDB extends RoomDatabase {
+
+    private static RoomDB database;
+    private static String DATABASE_NAME = "MyDB";
+
+    public synchronized static RoomDB getInstance(Context context){
+        if (database == null){
+            database = Room.databaseBuilder(context.getApplicationContext(),
+                            RoomDB.class, DATABASE_NAME).allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return database;
+    }
+    public abstract ItemsDAO mainDAO();
+}
